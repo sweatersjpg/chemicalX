b2f2904e42fc925b6a7d29b339f59b19aa8324fb
Sam <sweatersjpg@gmail.com>
2020-10-09 10:41:05 -0400

    added everything

---
+++ Game.js
@@ -0,0 +1,218 @@
+// Co-op game
+// sweatersjpg
+
+let DEBUG = false;
+
+let data = { password:{ secret:"chemicalx"}}
+
+function init_() {
+  setSpriteSheet("spriteSheet");
+  setNumberOfLayers(6);
+  lset(1);
+
+  pause_Button_.paused = false;
+  drawFN = new Game();
+
+  disableScroll();
+
+}
+
+// ------- main loops -------
+
+function Game(level) {
+  this.output = "booting -";
+
+  this.input = createInput('');
+  // this.input.hide();
+  this.input.elt.onblur = () => { this.input.focused = false }
+  this.input.elt.onfocus = () => { this.input.focused = true }
+
+  this.input.elt.focus();
+  this.input.position(-windowWidth,-windowHeight);
+
+  this.arguments = [];
+
+  this.setFunction = (fn, wait) => {
+    this.function = fn;
+    this.s = 0;
+    this.wait = wait;
+  }
+
+  this.s = 0;
+  this.function;
+  this.wait = false;
+
+  // functions
+
+  this.boot = () => {
+    if(this.s < 90) {
+      if(frameCount%2 == 0) {
+        if(this.output == "booting \\") this.output = "booting |";
+        else if(this.output == "booting |") this.output = "booting /";
+        else if(this.output == "booting /") this.output = "booting -";
+        else if(this.output == "booting -") this.output = "booting \\";
+      }
+    } else if(this.s == 90) {
+      this.output = "Boot successful~nLoading database~n";
+    } else if(this.s <= 140) {
+      this.print("_");
+    } else if(this.s <= 140+30*5) {
+      let str = "";
+      for (var i = 0; i < 50; i++) str+=String.fromCharCode(floor(random(33,126)));
+      this.print(str);
+    } else if(this.s <= 290+8) {
+      this.output = "";
+      cls(1);
+    } else if(this.s <= 290+16) {
+      cls(0);
+    } else {
+      this.setFunction(this.getUsername, true);
+      this.output = "Enter username:~n"
+    }
+  }
+  this.getUsername = (username) => {
+    console.log(username);
+    if(username.toLowerCase() == "redcat") {
+      cls(1);
+      this.output = "Username valid.~nEnter password for redcat:~n";
+      this.setFunction(this.getPassword, true);
+    } else {
+      cls(16);
+      this.output += "ERROR: BAsE64 cmVkY2F0~nUsername Incorrect~nEnter username:~n";
+    }
+  }
+  this.getPassword = (password) => {
+    if(password.toLowerCase() == "c8hf15O2") {
+
+    } else {
+      cls(16);
+      this.output += "ERROR: UEZBUyBmb3JtdWxh~nPassword not correct~n"
+    }
+  }
+
+  this.setFunction(this.boot, false);
+
+  // main loop
+
+  this.draw = () => {
+    cls(0);
+    textc(1);
+
+    put(this.output, 0, 0);
+
+    if(!this.wait) {
+      this.function();
+      this.input.elt.value = "";
+      this.s++;
+    }
+
+    if(getHeight(this.output) >= 240) this.output = trimFirst(this.output);
+
+    if(!this.wait) return;
+    put("> ", 0, getHeight(this.output));
+    setInput(this.input);
+    put(this.input.value());
+  }
+
+  this.print = (value) => {
+    this.output += value;
+  }
+
+  this.println = (value) => {
+    this.output += value + "~n";
+  }
+
+  this.keydown = (e) => {
+    if(e.keyCode == 13) {
+      this.output += "> " + this.input.value() + "~n";
+
+      if(this.wait) {
+        let args = this.input.value().split(' ');
+        args.filter(x => x !== '');
+
+        this.function(...args);
+      }
+
+      this.input.elt.value = "";
+    }
+  }
+
+  this.mousedown = (e) => {
+    e.preventDefault();
+    this.input.elt.focus();
+  }
+
+}
+
+function trimFirst(str) {
+  let sub = "";
+
+  for (var i = 0; i < str.length; i++) {
+    sub += str.charAt(i);
+    if(i > 0) {
+      if(str.charAt(i) == 'n' && str.charAt(i-1) == '~') break;
+    }
+    if(sub.length == 50) break;
+  }
+  str = str.replace(sub, '');
+  return str;
+}
+
+function getHeight(str, x) {
+  x = x || 0;
+  let count = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if(str.charAt(i) == "~") {
+      if(str.charAt(i+1) == "n") {
+        count += 1;
+        x = 0;
+        i++;
+        continue;
+      }
+    }
+    x++;
+    if(x > 50) {
+      x = 0;
+      count++;
+    }
+  }
+
+  return count * 8;
+
+}
+
+function changeStringRandom(str) {
+  str = str.split("");
+  str[floor(random(str.length))] = String.fromCharCode(floor(random(33,126)));
+  return str.join("");
+}
+
+let eventsToAdd = ['keydown', 'mousedown', 'dblclick', 'blur', 'focus', 'keyup', 'keypress'];
+for (let EVENT of eventsToAdd)
+window.addEventListener(EVENT, e => {
+  if(!pause_Button_.paused && drawFN && drawFN[EVENT]) drawFN[EVENT](e);
+});
+
+function disableScroll() {
+  var keys = {37: 0, 38: 1, 39: 0, 40: 1};
+  function preventDefault(e) { e.preventDefault(); }
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+  var supportsPassive = false;
+  try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+      get: function () { supportsPassive = true; }
+    }));
+  } catch(e) {}
+  var wheelOpt = supportsPassive ? { passive: false } : false;
+  var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
---
+++ put.js
@@ -0,0 +1,62 @@
+
+let putInput = false;
+
+function setInput(input) {
+  putInput = input;
+}
+
+function put(s, x, y, c) {
+  if(typeof x === 'undefined') x = pointer_.x;
+  else pointer_.ox = x;
+  if(typeof y === 'undefined') y = pointer_.y;
+  c = c || pointer_.c;
+  if(typeof c == 'string') c = parseInt(c, 16);
+  else s = String(s);
+  let dy = 0;
+  let dx = 0;
+  let sw = 8;
+  let dw = sw*D.S;
+  let oldC = c;
+  for (var i = 0; i <= s.length; i++) {
+    if(putInput && putInput.focused) {
+      c = oldC;
+      if(i >= putInput.elt.selectionStart && i < putInput.elt.selectionEnd) {
+        c = 0;
+        palset([1,1,1,1,1]);
+        spr(0, x+dx, y+dy, 1, 1, false, 0, 8, 8);
+      } else if(i == putInput.elt.selectionStart && frameCount % 5 > 5/2) {
+        c = 0;
+        palset([1,1,1,1,1]);
+        spr(0, x+dx, y+dy, 1, 1, false, 0, 8, 8);
+      }
+    }
+    if(i == s.length) continue;
+
+    let ch = s.charCodeAt(i) - 32;
+    if(ch == -1) continue;
+    let sy = floor(ch/32)*8;
+    let sx = (ch%32)*8;
+    if(s.charAt(i) == "~"){
+      if(s.charAt(i+1) == 'n') {
+        dx=0;
+        x=pointer_.ox;
+        dy+=sw;
+        i += 1;
+        continue;
+      }
+    }
+    temp = {img:font[c],w:dw,h:dw,sx:sx,sy:sy,sw:sw,sh:sw,camera: Object.assign({}, camera)};
+    temp.x = floor(x+dx)*D.S;
+    temp.y = floor(y+dy)*D.S;
+    layer[currentLayer].push(temp);
+    dx+=sw;
+    if(dx+x > 400-8){
+      dx = 0;
+      dy += sw;
+    }
+  }
+  pointer_.x = dx + pointer_.ox;
+  pointer_.y = dy + y;
+  pointer_.c = c;
+  putInput = false;
+}
